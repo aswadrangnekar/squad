59ace433a61aa986df100c8cb9ecd36f1eec545f
Aswad Rangnekar <aswad.r@gmail.com>
2012-05-12 14:24:26 +0530

    Remove moovida and chrome installations

--- shell-squad/basic.sh
+++ shell-squad/basic.sh
@@ -6,16 +6,11 @@ echo ""
 
 sudo apt-get install aptitude
 sudo aptitude install gnome-do gnome-do-plugins
-sudo aptitude install moovida vlc
-sudo aptitude install chromium chromium-browser moonlight-plugin-chromium
+sudo aptitude install vlc
+# sudo aptitude install chromium chromium-browser moonlight-plugin-chromium
 sudo aptitude install emacs
 
 clear
-echo "All the below app packages were installed"
-echo "1. aptitude"
-echo "2. gnome do and plugiins"
-echo "3. Meda center: Moovida and vlc"
-echo "4. Browser: chromium and plugins"
-echo "5: Editors: emacs"
+echo "Packages installed!"
 
 
